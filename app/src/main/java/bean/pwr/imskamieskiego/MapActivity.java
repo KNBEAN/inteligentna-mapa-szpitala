f74093d55f13a357f9fe4499e3909a62b09a037a
Anna Lasota <annlas44200@gmail.com>
2018-07-29 03:11:27 +0200

    Tools Button with animation added, but have to be fixed

--- app/src/main/java/bean/pwr/imskamieskiego/MapActivity.java
+++ app/src/main/java/bean/pwr/imskamieskiego/MapActivity.java
@@ -3,6 +3,7 @@ package bean.pwr.imskamieskiego;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.text.Layout;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,7 +13,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+
+import static android.view.View.GONE;
 
 public class MapActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,6 +32,45 @@ public class MapActivity extends AppCompatActivity
         setContentView(R.layout.activity_map);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Animations declaration
+        final Animation rotateShow = AnimationUtils.loadAnimation(MapActivity.this, R.anim.rotate_show);
+        final Animation rotateHide = AnimationUtils.loadAnimation(MapActivity.this, R.anim.rotate_hide);
+        final Animation hideButtonAnimation = AnimationUtils.loadAnimation(MapActivity.this, R.anim.hide_anim);
+        final Animation showButtonAnimation = AnimationUtils.loadAnimation(MapActivity.this, R.anim.show_anim);
+
+
+        // Initialization of buttons from content_map.xml
+        final FloatingActionButton wcButton = findViewById(R.id.wc_button);
+        final FloatingActionButton patientAssistantButton = findViewById(R.id.patient_assistant_button);
+        final FloatingActionButton foodButton = findViewById(R.id.food_button);
+        final FloatingActionButton toolsButton = findViewById(R.id.tools_button);
+        toolsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (foodButton.getVisibility() == View.VISIBLE &&
+                        (wcButton.getVisibility() == View.VISIBLE)) {
+                    wcButton.setVisibility(GONE);
+                    foodButton.setVisibility(GONE);
+                    patientAssistantButton.setVisibility(GONE);
+                    toolsButton.startAnimation(rotateHide);
+                    wcButton.startAnimation(showButtonAnimation);
+                    foodButton.startAnimation(showButtonAnimation);
+                    patientAssistantButton.startAnimation(showButtonAnimation);
+                }
+                else {
+                    wcButton.setVisibility(View.VISIBLE);
+                    foodButton.setVisibility(View.VISIBLE);
+                    patientAssistantButton.setVisibility(View.VISIBLE);
+                    toolsButton.startAnimation(rotateShow);
+                    wcButton.startAnimation(hideButtonAnimation);
+                   foodButton.startAnimation(hideButtonAnimation);
+                   patientAssistantButton.startAnimation(hideButtonAnimation);
+                }
+            }
+        });
+
+
 
 
 
