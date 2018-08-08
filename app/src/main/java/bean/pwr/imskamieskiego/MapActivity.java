cca5fd5acf9a76b51958de658630beb62ee8e0ea
Piotrek1697 <30784285+Piotrek1697@users.noreply.github.com>
2018-08-08 10:28:12 +0200

    checkpoint

--- app/src/main/java/bean/pwr/imskamieskiego/MapActivity.java
+++ app/src/main/java/bean/pwr/imskamieskiego/MapActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 import bean.pwr.imskamieskiego.NavigationWindow.NavWindowFragment;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity {
 
     Button button;
     Fragment navWindowFragment;
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_map);
 
         button = (Button) findViewById(R.id.button);
 
