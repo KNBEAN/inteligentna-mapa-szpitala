b0aae8035e0af24768b933758d48c4575621989d
Anna Lasota <annlas44200@gmail.com>
2018-11-12 23:43:19 +0100

    Popup window instead popup menu (on floor change button click)

--- app/src/main/java/bean/pwr/imskamieskiego/MapActivity.java
+++ app/src/main/java/bean/pwr/imskamieskiego/MapActivity.java
@@ -1,16 +1,21 @@
 package bean.pwr.imskamieskiego;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.PorterDuff;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.ActionMenuItem;
+import android.support.v7.view.menu.MenuItemImpl;
 import android.util.Log;
+import android.view.ActionProvider;
 import android.view.ContextThemeWrapper;
 import android.view.Menu;
 
@@ -22,13 +27,23 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.PopupMenu;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 import android.widget.Toast;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import bean.pwr.imskamieskiego.GUI.InfoSheet;
 import bean.pwr.imskamieskiego.GUI.locationSearch.SearchFragment;
@@ -67,9 +82,7 @@ public class MapActivity extends AppCompatActivity
     private FloorViewModel floorViewModel;
     private MapDrawer mapDrawer;
 
-
     private int currentFloor = 1;
-
 
 
     @Override
@@ -100,29 +113,15 @@ public class MapActivity extends AppCompatActivity
 
         changeFloorButton = findViewById(R.id.floors_button);
         changeFloorButton.setText(Integer.toString(currentFloor));
-        Context wrapper = new ContextThemeWrapper(this, R.style.CustomPopupMenu);
-        PopupMenu floorSelect = new PopupMenu(wrapper, changeFloorButton);
-        floorViewModel.getFloorList().observe(MapActivity.this, floorList -> {
-            if (floorList != null) {
-                for (int i = 0; i < floorList.length; i++) {
-                    floorSelect.getMenu().add(1,i,i,floorList[i]);
-                }
+        changeFloorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayPopupWindow(v);
             }
         });
 
-        changeFloorButton.setOnClickListener(v -> floorSelect.show());
 
-        floorSelect.setOnMenuItemClickListener(item -> {
-            if (currentFloor != item.getItemId()) {
-                currentFloor = item.getItemId();
-                floorViewModel.setCurrentFloor(currentFloor);
-                floorViewModel.setSelectedFloor(currentFloor);
-                changeFloorButton.setText(Integer.toString(currentFloor));
-            }
-            else
-                Toast.makeText(MapActivity.this, item.getTitle().toString(), Toast.LENGTH_LONG).show();
-            return false;
-        });
+
 
 
         DrawerLayout drawerLayout = findViewById(R.id.mainDrawerLayout);
@@ -139,8 +138,8 @@ public class MapActivity extends AppCompatActivity
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-    }
 
+    }
 
 
     @Override
@@ -148,10 +147,9 @@ public class MapActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.mainDrawerLayout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else if (quickAccessFragment.isAdded() && quickAccessFragment.isExpanded()){
+        } else if (quickAccessFragment.isAdded() && quickAccessFragment.isExpanded()) {
             quickAccessFragment.hideQuickAccessButtons();
-        }
-        else {
+        } else {
             super.onBackPressed();
         }
     }
@@ -165,7 +163,7 @@ public class MapActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
-        if (itemId == R.id.searchMenuItem){
+        if (itemId == R.id.searchMenuItem) {
             displaySearchFragment();
         }
         return super.onOptionsItemSelected(item);
@@ -180,7 +178,7 @@ public class MapActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_authors) {
 
-            Intent intent = new Intent(this,AuthorsActivity.class);
+            Intent intent = new Intent(this, AuthorsActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_help) {
@@ -206,7 +204,6 @@ public class MapActivity extends AppCompatActivity
     }
 
 
-
     private void viewModelInit() {
         locationViewModel = ViewModelProviders.of(this).get(LocationViewModel.class);
         locationViewModel.getCurrentLocation().observe(this, location -> {
@@ -218,18 +215,19 @@ public class MapActivity extends AppCompatActivity
         locationViewModel.getNearestMapPoint().observe(this, mapPoint -> {
                     if (mapPoint != null) {
                         mapDrawer.zoomOnPoint(mapPoint);
-                        Log.i(TAG,"x= "+mapPoint.getX()+"y: "+mapPoint.getY()+"floor: "+mapPoint.getFloor());
-                        mapDrawer.addMapPoint(mapPoint,1);
+                        Log.i(TAG, "x= " + mapPoint.getX() + "y: " + mapPoint.getY() + "floor: " + mapPoint.getFloor());
+                        mapDrawer.addMapPoint(mapPoint, 1);
                     }
                 }
         );
 
     }
+
     private void floorViewModelInit() {
         floorViewModel = ViewModelProviders.of(this).get(FloorViewModel.class);
         floorViewModel.getFloorBitmap().observe(this, bitmap -> {
                     if (bitmap != null) {
-                        mapDrawer.showFloor(currentFloor,bitmap);
+                        mapDrawer.showFloor(currentFloor, bitmap);
 
                     }
                 }
@@ -238,42 +236,44 @@ public class MapActivity extends AppCompatActivity
     }
 
 
-
-
-    private void displaySearchFragment(){
+    private void displaySearchFragment() {
 
         FragmentTransaction fTransaction = fragmentManager.beginTransaction();
-        if(searchFragment.isAdded()){
+        if (searchFragment.isAdded()) {
             fTransaction.show(searchFragment);
-        }else {
+        } else {
             fTransaction.add(R.id.mainDrawerLayout, searchFragment, searchFragmentTag);
         }
 
         fTransaction.addToBackStack(null).commit();
     }
 
-    private void displayInfoSheet(Location location, boolean asStartPoint){
+    private void displayInfoSheet(Location location, boolean asStartPoint) {
         FragmentTransaction fTransaction = fragmentManager.beginTransaction();
 
         fTransaction.replace(R.id.infoSheetStub, InfoSheet.newInstance(location, asStartPoint), infoSheetTag);
 
-        if (quickAccessFragment.isAdded() && !quickAccessFragment.isHidden()){fTransaction.hide(quickAccessFragment);}
+        if (quickAccessFragment.isAdded() && !quickAccessFragment.isHidden()) {
+            fTransaction.hide(quickAccessFragment);
+        }
 
         fTransaction.addToBackStack(null).commit();
     }
 
-    private void displayNavigationSetup(String destinationLocationName){
+    private void displayNavigationSetup(String destinationLocationName) {
         FragmentTransaction fTransaction = fragmentManager.beginTransaction();
 
-        fTransaction.setCustomAnimations(R.anim.slide_in_from_left,android.R.anim.slide_out_right,
+        fTransaction.setCustomAnimations(R.anim.slide_in_from_left, android.R.anim.slide_out_right,
                 R.anim.slide_in_from_left, android.R.anim.slide_out_right);
         NavigationSetupFragment navigationSetupFragment = NavigationSetupFragment.newInstance(destinationLocationName);
         fTransaction.replace(R.id.toolBarHolder, navigationSetupFragment, navigationSetupTag);
 
-        if (quickAccessFragment.isAdded() && !quickAccessFragment.isHidden()){fTransaction.hide(quickAccessFragment);}
+        if (quickAccessFragment.isAdded() && !quickAccessFragment.isHidden()) {
+            fTransaction.hide(quickAccessFragment);
+        }
 
         Fragment infoSheetFragment = fragmentManager.findFragmentByTag(infoSheetTag);
-        if (infoSheetFragment != null){
+        if (infoSheetFragment != null) {
             fTransaction.hide(infoSheetFragment);
         }
 
@@ -285,11 +285,11 @@ public class MapActivity extends AppCompatActivity
 
         fTransaction.addToBackStack(navigationSetupTag).commit();
     }
-    
+
 
     @Override
     public void onQAButtonClick(QuickAccessFragment.QuickAccessButtons button) {
-        switch (button){
+        switch (button) {
             case WC:
                 Log.d(TAG, "Quick access WC");
                 displayNavigationSetup("potato");
@@ -317,7 +317,7 @@ public class MapActivity extends AppCompatActivity
 
     @Override
     public void infoSheetAction(boolean asStartPoint) {
-        if(asStartPoint){
+        if (asStartPoint) {
             Log.i(TAG, "infoSheetAction: SELECTED");
             displayNavigationSetup("Tomato");
         }
@@ -333,5 +333,78 @@ public class MapActivity extends AppCompatActivity
     @Override
     public void startNavigation(boolean avoidStairs) {
         Log.i(TAG, "startNavigation: start navigation");
+    }
+
+    private void displayPopupWindow(View anchorView) {
+
+        PopupWindow popupWindow = new PopupWindow(MapActivity.this);
+        View popupLayout = getLayoutInflater().inflate(R.layout.choose_floor_list, null);
+        ListView floorListView=popupLayout.findViewById(R.id.floor_list);
+        floorListView.setAdapter(floorsAdapter());
+        popupWindow.setContentView(popupLayout);
+        floorListView.setOnItemClickListener(
+
+                (parent, view, position, id) -> {
+                    if (currentFloor != position) {
+                        currentFloor = position;
+                        view.setBackgroundColor(getColor(R.color.colorPrimary));
+
+
+                        floorViewModel.setCurrentFloor(currentFloor);
+                        floorViewModel.setSelectedFloor(currentFloor);
+                        changeFloorButton.setText(Integer.toString(currentFloor));
+
+                    }
+                });
+
+//
+//
+//        floorSelect.setOnMenuItemClickListener(item -> {
+//
+//            if (currentFloor != item.getItemId()) {
+//                currentFloor = item.getItemId();
+//
+////                item.setEnabled(false);
+//                floorViewModel.setCurrentFloor(currentFloor);
+//                floorViewModel.setSelectedFloor(currentFloor);
+//                changeFloorButton.setText(Integer.toString(currentFloor));
+//                TextView tv = new TextView(MapActivity.this);
+//                tv.setText(item.getTitle());
+//                tv.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+//
+//            }
+//            else
+//                Toast.makeText(MapActivity.this, item.getTitle().toString(), Toast.LENGTH_LONG).show();
+//            return false;
+//        });
+
+
+        popupWindow.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+        popupWindow.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
+//        popupWindow.setHeight(50);
+//        popupWindow.setWidth(50);
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.setFocusable(true);
+
+
+        popupWindow.showAsDropDown(anchorView);
+
+
+    }
+
+    ArrayAdapter<String> floorsAdapter() {
+        ArrayList <String> floors = new ArrayList<>();
+
+        floorViewModel.getFloorList().observe(MapActivity.this, floorList -> {
+            if (floorList != null) {
+                for (int i = 0; i < floorList.length; i++) {
+                    floors.add(floorList[i]);
+
+                }
+            }
+        });
+        ArrayAdapter<String> floorAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, floors);
+
+        return floorAdapter;
     }
 }
