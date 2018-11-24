6d84d2ad186bbad689b5158eed122db2b215f278
Karol Wyskocki <karol.wyskocki@vp.pl>
2018-11-24 20:52:52 +0100

    the keyboard is displayed automatically when the search fragment open

--- app/src/main/java/bean/pwr/imskamieskiego/GUI/locationSearch/SearchFragment.java
+++ app/src/main/java/bean/pwr/imskamieskiego/GUI/locationSearch/SearchFragment.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -41,6 +42,7 @@ public class SearchFragment extends Fragment implements SearchView.OnQueryTextLi
         searchViewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
         searchViewModel.getSuggestionsList().observe(this, this::createSuggestions);
         searchViewModel.getSubmitQueryResult().observe(this, this::processSearchResult);
+
     }
 
     @Override
@@ -71,6 +73,14 @@ public class SearchFragment extends Fragment implements SearchView.OnQueryTextLi
         searchView.setOnQueryTextListener(this);
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        searchView.requestFocus();
+        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, InputMethodManager.HIDE_IMPLICIT_ONLY);
     }
 
     @Override
